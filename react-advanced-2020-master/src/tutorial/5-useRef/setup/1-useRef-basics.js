123abce42c06a25c0f6ba0594378626effbe0b22
rendinel <rendinel@protonmail.com>
2021-08-21 18:34:40 +0200

    push

--- react-advanced-2020-master/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ react-advanced-2020-master/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,40 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
-};
+  const refContainer = useRef(null)
+  const divContainer = useRef(null)
 
-export default UseRefBasics;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //this way we grab the value we submit from the input
+    console.log(refContainer.current.value)
+    //we can use to grab everything insdie the dom,in this case we are grabbing the div
+    console.log(divContainer.current)
+  }
+  //it's an object with a key of current and the default value we choose null in this case (current:null)
+  // console.log(refContainer)
+
+  //because it doen't trigger the rerender we don't to pass the [], it will only work the first time the page load
+  useEffect(() => {
+    console.log(refContainer.current)
+    refContainer.current.focus()
+  })
+
+  return (
+    <>
+      <form className='form' onSubmit={handleSubmit}>
+        <div>
+          <input type='text' ref={refContainer} />
+          <button type='submit'>submit</button>
+        </div>
+        <div ref={divContainer}>hello world</div>
+      </form>
+    </>
+  )
+}
+
+export default UseRefBasics
--- react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,45 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from 'react'
+import Modal from './Modal'
+import { data } from '../../../data'
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
-};
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+  const [showModal, setShowModal] = useState(false)
 
-export default Index;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name) {
+      setShowModal(true)
+      setPeople([...people, { id: new Date().getTime().toString(), name }])
+      setName('')
+    } else {
+      setShowModal(true)
+    }
+  }
+  return (
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit} className='form'>
+        <div>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type='text'
+          />
+        </div>
+        <button type='submit'>add</button>
+      </form>
+      {people.map((person) => {
+        return (
+          <div key={personalbar.id}>
+            <h4>{person.name}</h4>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Index
